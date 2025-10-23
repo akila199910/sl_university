90a0cd9f24e0dc57b3437375c1031930db6046ab
Akila Umayanga <akeeumayanga7775@gmail.com>
2025-10-23 11:56:59 +0530

    create navbar component

--- sluniversityfrontend/src/components/Header/Navbar.tsx
+++ sluniversityfrontend/src/components/Header/Navbar.tsx
@@ -1,13 +1,35 @@
+"use client"
 import React from 'react'
+import { manuTemItems } from './menuItem'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='flex justify-around items-center h-16 bg-gray-800 text-white'>
-        <p>A</p>
-        <p>B</p>
-        <p>C</p>
-        <p>D</p>
-        <p>E</p>
+    <div className='flex justify-between items-center px-4 sm:px-6 md:px-8 lg:px-12 py-4 bg-amber-50 shadow-md '>
+
+      {/* navbar logo and app name */}
+        <div className='flex items-center gap-4'>
+          <div className=''>APP Logo</div>
+          <div className='font-bold text-2xl'>App Name</div>
+        </div>
+
+        {/* navbar menu items */}
+        <div className="flex gap-3">
+          {manuTemItems.map((item, index) => {
+            return (
+              <Link
+                key={index}
+                href={item.link}
+                className="text-gray-700 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </div>
+        {/* user profile icon */}
+          <div className='rounded-full bg-amber-500 p-2 uppercase cursor-pointer'>AU</div>
+      
     </div>
   )
 }
---
+++ sluniversityfrontend/src/components/Header/menuItem.ts
@@ -0,0 +1,6 @@
+export const manuTemItems = [
+    { id: 1, name: 'Home', link: '/' },
+    { id: 2, name: 'About', link: '/about' },
+    { id: 3, name: 'Services', link: '/services' },
+    { id: 4, name: 'Contact', link: '/contact' },
+];
