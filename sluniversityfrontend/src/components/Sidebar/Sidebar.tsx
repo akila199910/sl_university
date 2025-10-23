7298500086b30b373ed861ee9b0aaf32509d933d
Akila Umayanga <akeeumayanga7775@gmail.com>
2025-10-23 16:33:00 +0530

    sidebar link and nested link

--- sluniversityfrontend/src/components/Sidebar/Sidebar.tsx
+++ sluniversityfrontend/src/components/Sidebar/Sidebar.tsx
@@ -1,15 +1,64 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { MouseEventHandler, useState } from 'react'
+import sidebarLinks from './sidebarLink'
 
 const Sidebar = () => {
+
+  const [openSublinks, setOpenSublinks] = useState<number[]>([]);
+
+  const handleShow = (e: React.MouseEvent<HTMLDivElement>, linkId: number) => {
+    e.preventDefault();
+    setOpenSublinks(prev =>
+      prev.includes(linkId)
+        ? prev.filter(id => id !== linkId)
+        : [...prev, linkId]
+    );
+  }
   return (
-        <>
-            <Link className='flex items-center gap-2 cursor-pointer
-             bg-blue-300 rounded-md mb-2 hover:bg-blue-800' href={"/"}>
-                <div className='p-2'>IC</div>
-                <div className='p-2 text-start'>Name</div>
-            </Link>
-        </>
+    <>
+      {
+        sidebarLinks.map((sidebarLink) => {
+
+          if (sidebarLink.multy !== null && sidebarLink.multy === true) {
+            return (
+              <div key={sidebarLink.id} className='flex flex-col'>
+                <div className='flex items-center gap-2 cursor-pointer
+               bg-blue-300 rounded-md mb-2 hover:bg-blue-800 p-2'>
+                  <div className='text-start'>{sidebarLink.icon}</div>
+                  <div className='text-start'>{sidebarLink.name}</div>
+                  <div className='text-start' onClick={(e) => { handleShow(e, sidebarLink.id) }}>Icon</div>
+                </div>
+                <div className='bg-red-900 mb-2 rounded-md'>
+                  {
+                    sidebarLink.subLinks && sidebarLink.subLinks.map((subLink) => (
+                      <div key={subLink.id} className={` ${openSublinks.includes(sidebarLink.id) ? '' : 'hidden'} mb-`}>
+                        <Link key={subLink.id} className='flex items-center gap-2 cursor-pointer
+                        p-2 ml-4' href={subLink.link}>
+                          <div className='text-start'>{subLink.icon}</div>
+                          <div className='text-start'>{subLink.name}</div>
+                        </Link>
+                      </div>
+
+                    ))
+                  }
+                </div>
+
+              </div>
+
+            );
+          } else {
+            return (
+              <Link key={sidebarLink.id} className='flex items-center gap-2 cursor-pointer
+               bg-blue-300 rounded-md mb-2 hover:bg-blue-800 p-2' href={sidebarLink.link}>
+                <div className='text-start'>{sidebarLink.icon}</div>
+                <div className='text-start'>{sidebarLink.name}</div>
+              </Link>
+            );
+          }
+
+        })}
+    </>
   )
 }
 
---
+++ sluniversityfrontend/src/components/Sidebar/sidebarLink.ts
@@ -0,0 +1,28 @@
+const sidebarLinks = [
+    { id: 1, name: 'Dashboard', link: '/dashboard' , icon: 'DA'},
+    { id: 2, name: 'Profile', link: '/profile' , icon: 'PR'},
+    { id: 3, name: 'Settings', link: '/settings', icon: 'SE' },
+    { id: 4, name: 'Help', link: '/help', icon: 'HE' },
+    { id: 5, name: 'About', link: '/about', icon: 'AB' },
+    { id: 6, name: 'Logout', link: '/logout', icon: 'LO' },
+    { id: 15, name: 'Users', link: '/users', icon: 'US', multy: true,
+        subLinks: [
+            { id: 1, name: 'Admins', link: '/admins', icon: 'AU' },
+            { id: 2, name: 'Users', link: '/users', icon: 'Us' },
+        ]
+     },
+    { id: 7, name: 'Dashboard', link: '/dashboard' , icon: 'DA', multy: true,
+        subLinks: [
+            { id: 1, name: 'Admins', link: '/admins', icon: 'AU' },
+            { id: 2, name: 'Users', link: '/users', icon: 'Us' },
+        ]},
+    { id: 8, name: 'Profile', link: '/profile' , icon: 'PR'},
+    { id: 9, name: 'Settings', link: '/settings', icon: 'SE' },
+    { id: 10, name: 'Help', link: '/help', icon: 'HE' },
+    { id: 11, name: 'About', link: '/about', icon: 'AB' },
+    { id: 12, name: 'Logout', link: '/logout', icon: 'LO' },
+    { id: 13, name: 'Logout', link: '/logout', icon: 'LO' },
+    { id: 14, name: 'Logout', link: '/logout', icon: 'LO' },
+];
+
+export default sidebarLinks;
