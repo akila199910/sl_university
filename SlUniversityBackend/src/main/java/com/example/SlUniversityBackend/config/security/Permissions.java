ad874b78358e0e3a332e6e64b5590a268a36f8a4
Akila Umayanga <akeeumayanga7775@gmail.com>
2025-11-01 21:18:18 +0530

    Update Permissions.java

--- SlUniversityBackend/src/main/java/com/example/SlUniversityBackend/config/security/Permissions.java
+++ SlUniversityBackend/src/main/java/com/example/SlUniversityBackend/config/security/Permissions.java
@@ -6,14 +6,18 @@ public final class Permissions {
     public static final String ADMIN_CREATE = "ADMIN_CREATE";
     public static final String ADMIN_UPDATE = "ADMIN_UPDATE";
     public static final String ADMIN_DELETE = "ADMIN_DELETE";
-    public static final String ROLE_READ = "ROLE_READ"; // Add new permissions
+
+
+    public static final String ROLE_READ = "ROLE_READ";
     public static final String ROLE_CREATE = "ROLE_CREATE";
     public static final String ROLE_UPDATE = "ROLE_UPDATE";
     public static final String ROLE_DELETE = "ROLE_DELETE";
-    public static final String PERMISSION_READ = "PERMISSION_READ"; // Add new permissions
-    public static final String PERMISSION_CREATE = "PERMISSION_CREATE";
-    public static final String PERMISSION_UPDATE = "PERMISSION_UPDATE";
-    public static final String PERMISSION_DELETE = "PERMISSION_DELETE";
+
+
+//    public static final String PERMISSION_READ = "PERMISSION_READ";
+//    public static final String PERMISSION_CREATE = "PERMISSION_CREATE";
+//    public static final String PERMISSION_UPDATE = "PERMISSION_UPDATE";
+//    public static final String PERMISSION_DELETE = "PERMISSION_DELETE";
 
 
     public static final String USER_READ = "USER_READ";
@@ -21,6 +25,4 @@ public final class Permissions {
     public static final String USER_UPDATE = "USER_UPDATE";
     public static final String USER_DELETE = "USER_DELETE";
 
-
-    // ... add any other permissions needed
 }
