d9497b8612c82d59a43f8f113a858878619d64c0
Akila Umayanga <akeeumayanga7775@gmail.com>
2025-11-08 21:00:09 +0530

    Update SlUniversityBackendApplication.java

--- SlUniversityBackend/src/main/java/com/example/SlUniversityBackend/SlUniversityBackendApplication.java
+++ SlUniversityBackend/src/main/java/com/example/SlUniversityBackend/SlUniversityBackendApplication.java
@@ -1,6 +1,5 @@
 package com.example.SlUniversityBackend;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SlUniversityBackendApplication {
 
 	public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
+
 		SpringApplication.run(SlUniversityBackendApplication.class, args);
 	}
 
